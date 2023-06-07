dca7dba98587178529e4b0bec950ef67d44391cd
ouhassou <ouhassou.abdelkebir@yobeen.com>
2023-06-07 10:16:22 +0100

    edit : service page

--- tailwind-css/config.js
+++ tailwind-css/config.js
@@ -1,11 +1,11 @@
-
 tailwind.config = {
-    theme: {
-      extend: {
-        colors: {
-          primary: "#339ED3",
-          secondary : "#F5F5F5"
-        },
+  theme: {
+    extend: {
+      colors: {
+        primary: "#339ED3",
+        secondary: "#F5F5F5",
+        tertiary: "rgba(0, 0, 0, 0.7)",
       },
     },
-  };
+  },
+};
